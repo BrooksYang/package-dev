da3511754aaa73d952c98edece221a8ccc1db621
BrooksYang <yangchangqing@easy2study.net>
2017-08-16 16:04:11 +0800

    添加group

---
+++ packages/brooksyang/entrance/src/Entrance/Models/Group.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrooksYang\Entrance\Models;
+
+use BrooksYang\Entrance\Traits\EntranceGroupTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use EntranceGroupTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * Creates a new instance of the model.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = config('entrance.groups_table');
+    }
+}
--- packages/brooksyang/entrance/src/Entrance/Models/Module.php
+++ packages/brooksyang/entrance/src/Entrance/Models/Module.php
@@ -27,4 +27,16 @@ class Module extends Model implements ModuleInterface
         parent::__construct($attributes);
         $this->table = config('entrance.modules_table');
     }
+
+    /**
+     * keyword search
+     *
+     * @param $query
+     * @param $keyword
+     * @return mixed
+     */
+    public function scopeSearch($query, $keyword)
+    {
+        return $query->where('name', 'like', "%$keyword%");
+    }
 }
