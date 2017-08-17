843266417d10c99f52cc3dedaac1cde31ce3b97d
BrooksYang <yangchangqing@easy2study.net>
2017-08-17 10:41:42 +0800

    资源文件

---
+++ packages/brooksyang/entrance/src/Admin/views/layouts/include/breadcrumb.blade.php
@@ -0,0 +1,21 @@
+<div class="sub-board">
+    <span class="header-icon"><i class="fontello-home"></i>
+    </span>
+
+    {{-- Bredcrumb --}}
+    <ol class="breadcrumb newcrumb ng-scope">
+        <li>
+            <a href="/"><span></span>dashboard</a>
+        </li>
+    </ol>
+
+    {{-- Search --}}
+    <div class="dark" style="visibility: visible;">
+        <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+                <input type="text" name="keyword" value="{{ Request::get('keyword') }}"
+                       class="form-control search rounded id_search" placeholder="Search">
+            </div>
+        </form>
+    </div>
+</div>
