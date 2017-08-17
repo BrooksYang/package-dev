5213f355f30a310917756ac2c1de6fed6046a849
BrooksYang <yangchangqing@easy2study.net>
2017-08-17 18:07:53 +0800

    菜单

--- packages/brooksyang/entrance/src/Admin/views/layouts/include/side_menu.blade.php
+++ packages/brooksyang/entrance/src/Admin/views/layouts/include/side_menu.blade.php
@@ -9,276 +9,49 @@
             <div id="tree-wrap">
                 <div class="side-bar">
                     <ul id="menu-showhide" class="topnav">
-                        <li class="devider-title">
-                            <h3>
-                                <span>Desain Kit Menu</span>
-                            </h3>
-                        </li>
-                        <li>
-                            <a style="border-left:4px solid #5F9BDB; padding:0 0 0 16px;" class="tooltip-tip"
-                               href="index.html" title="Dashboard">
-                                <i class="fontello-desktop-1"></i>
-                                <span>Dashboard</span>
+                        {{-- Groups --}}
+                        @foreach (Auth::user()->menu() as $group)
+                            <li class="devider-title">
+                                <h3>
+                                    <span>{{ $group->name }}</span>
+                                </h3>
+                            </li>
+                            <li>
+                                <a href="#" title="Dashboard">
+                                    <i class="fontello-desktop-1"></i>
+                                    <span>Dashboard</span>
+                                </a>
+                            </li>
 
-                            </a>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="#" title="Mail">
-                                <i class=" fontello-mail-1"></i>
-                                <span>mail</span>
-
-                            </a>
-                            <ul>
-                                <!--  <li class="hide-min-toggle">UI Element</li> -->
+                            {{-- Modules --}}
+                            @foreach ($group->modules as $module)
                                 <li>
-                                    <!-- class="active" -->
-                                    <a href="mail.html" title="Index">Inbox</a>
-                                </li>
-                                <li>
-                                    <!-- class="active" -->
-                                    <a href="compose.html" title="Compose">Compose</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="#" title="UI">
-                                <i class="fontello-note"></i>
-                                <span>UI</span>
-                            </a>
-                            <ul>
-                                <!--  <li class="hide-min-toggle">UI Element</li> -->
-                                <li>
-                                    <!-- class="active" -->
-                                    <a href="element.html" title="Element">Element</a>
-                                </li>
-                                <li><a href="button.html" title="Button">
-                                        Button
+                                    {{-- Module Title --}}
+                                    <a class="tooltip-tip" href="#" title="Layout">
+                                        <i class="{{ $module->icon }}"></i>
+                                        <span>{{ $module->name }}</span>
                                     </a>
-                                </li>
-                                <li>
-                                    <a href="wizard.html" title="Tab & Accordion">Wizard</a>
-                                </li>
-                                <li>
-                                    <a href="calendar.html" title="Calender">Calendar</a>
-                                </li>
-                                <li>
-                                    <a href="tree.html" title="Tree View">Tree View</a>
-                                </li>
-                                <li>
-                                    <a href="grids.html" title="Grids">Grids</a>
-                                </li>
-                                <li>
-                                    <a href="chart.html" title="Chart">Chart</a>
-                                </li>
-                                <li>
-                                    <a href="typhography.html" title="Typhoghrapy">
 
-                                        Typhoghrapy
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="#" title="Layout">
-                                <i class=" fontello-pencil-1"></i>
-                                <span>Layout&nbsp;
-                                            <small class="side-menu-noft bg-aqua">new</small>
-                                        </span>
-
-                            </a>
-                            <ul>
-                                <!--  <li class="hide-min-toggle">UI Element</li> -->
-                                <li>
-                                    <!-- class="active" -->
-                                    <a href="nopadding.html" title="Index">full width</a>
-                                </li>
-                                <li>
-                                    <!-- class="active" -->
-                                    <a href="minimize.html" title="Compose">minimize</a>
-                                </li>
-                                <li>
-                                    <!-- class="active" -->
-                                    <a href="rightmenu.html" title="Compose">Right Side bar</a>
-                                </li>
-                                <li>
-                                    <!-- class="active" -->
-                                    <a href="topmenu.html" title="Compose">Top menu</a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a class="tooltip-tip" href="#" title="Bg">
-                                <i class="fontello-photo"></i>
-                                <span>Background&nbsp;
-                                            <small class="side-menu-noft bg-aqua">new</small>
-                                        </span>
-
-                            </a>
-                            <ul>
-                                <li>
-                                    <a href="imagebg.html" title="Image">Image </a>
-                                </li>
-                                <li>
-                                    <a href="videobg.html" title="Video">Video </a>
-                                </li>
-
-                            </ul>
-                        </li>
-
-                        <li class="devider-horizontal"></li>
-                        <li class="devider-title">
-                            <h3>
-                                <span>Component</span>
-                            </h3>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="#" title="Form">
-                                <i class="fontello-doc-1"></i>
-                                <span>Form</span>
-                            </a>
-                            <ul>
-                                <li>
-                                    <a href="form-element.html" title="Form Elements">Form Elements</a>
-                                </li>
-                                <li>
-                                    <a href="andvance-form.html" title="Andvance Form">Andvance Form</a>
-                                </li>
-                                <li>
-                                    <a href="text-editor.html" title="Text Editor">Text Editor</a>
-                                </li>
-                                <li>
-                                    <a href="file-upload.html" title="File Upload">File Upload</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="#" title="Tables">
-                                <i class="fontello-calendar-1"></i>
-                                <span>Tables</span>
-                            </a>
-                            <ul>
-                                <li>
-                                    <a href="table-static.html" title="Table Static">Table Static</a>
-                                </li>
-                                <li>
-                                    <a href="table-dynamic.html" title="Table Dynamic">Table Dynamic</a>
+                                    {{-- Permissions --}}
+                                    @foreach ($module->permissions as $permission)
+                                        <ul>
+                                            <li>
+                                                <a href="{{ url($permission->uri) }}" title="Index">{{ $permission->name }}</a>
+                                            </li>
+                                        </ul>
+                                    @endforeach
+                                    {{-- Permissions End --}}
 
                                 </li>
+                            @endforeach
+                            {{-- Modules End --}}
 
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="icon.html" title="Icons">
-                                <i class="fontello-food"></i>
-                                <span>Icons</span>
-                                <div class="noft-blue bg-green" style="display: inline-block; float: none;">New</div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="map.html" title="Map">
-                                <i class="fontello-location-1"></i>
-                                <span>Map</span>
-
-                            </a>
-                        </li>
-                        <li class="devider-horizontal"></li>
-                        <li class="devider-title">
-                            <h3>
-                                <span>Special Page</span>
-                            </h3>
-                        </li>
-                        <li>
-                            <a class="tooltip-tip" href="#" title="Extra">
-                                <i class="fontello-beaker"></i>
-                                <span>Extra</span>
-                            </a>
-                            <ul>
-                                <li>
-                                    <a href="invoice.html" title="Invoice">Invoice</a>
-                                </li>
-                                <li>
-                                    <a href="pricing_table.html" title="Pricing Table">Pricing Table</a>
-                                </li>
-                                <li>
-                                    <a href="time-line.html" title="Time Line">Time Line</a>
-                                </li>
-                                <li>
-                                    <a href="404.html" title="404 Error Page">404 Error Page</a>
-                                </li>
-                                <li>
-                                    <a href="500.html" title="500 Error Page">500 Error Page</a>
-                                </li>
-                                <li>
-                                    <a href="lock-screen.html" title="Lock Screen">Lock Screen</a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a class="tooltip-tip " href="login.html" title="login">
-                                <i class=" fontello-lock-1"></i>
-                                <span>Login</span>
-                            </a>
-                        </li>
+                            <li class="devider-horizontal"></li>
+                        @endforeach
+                        {{-- Groups End --}}
                     </ul>
-                    <div class="side-dash">
-                        <h3>
-                            <span>Task Progress</span>
-                        </h3>
-                        <ul class="side-dashh-list">
-                            <li>
-                                <!-- Task item -->
-                                <a href="#">
-                                    <h3 class="fontello-leaf">
-                                        Tidy up your files
-                                        <small class="pull-right">35%</small>
-                                    </h3>
-                                    <div class="progress xs bg-opacity-one height-tiny">
-                                        <div class="progress-bar bg-blue " style="width: 35%" role="progressbar"
-                                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                            <span class="sr-only">35% Complete</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <!-- end task item -->
-                            <li>
-                                <!-- Task item -->
-                                <a href="#">
-                                    <h3 class="fontello-box">
-                                        Check server status
-                                        <small class="pull-right">40%</small>
-                                    </h3>
-                                    <div class="progress xs bg-opacity-one height-tiny">
-                                        <div class="progress-bar bg-aqua" style="width: 40%" role="progressbar"
-                                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                            <span class="sr-only">40% Complete</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <!-- end task item -->
-                            <li>
-                                <!-- Task item -->
-                                <a href="#">
-                                    <h3 class="fontello-headphones">
-                                        Support costumer
-                                        <small class="pull-right">60%</small>
-                                    </h3>
-                                    <div class="progress xs bg-opacity-one height-tiny">
-                                        <div class="progress-bar bg-red-orange" style="width: 60%" role="progressbar"
-                                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                            <span class="sr-only">60% Complete</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
 
-                        <h3>
-                            <span>Reply</span>
-                        </h3>
+                    <div class="side-dash">
                         <div id="doughnutChart" class="chart"></div>
                     </div>
                 </div>
