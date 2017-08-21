3efeba2dd195d78f0a25ca2bda27491f03d0c5cb
BrooksYang <yangchangqing@easy2study.net>
2017-08-21 15:40:17 +0800

    entrance::auth

--- packages/brooksyang/entrance/src/Admin/routes/entrance.php
+++ packages/brooksyang/entrance/src/Admin/routes/entrance.php
@@ -28,7 +28,21 @@ Route::group(['prefix' => 'auth', 'middleware' => ['web', 'auth', 'permission'],
 
 });
 
-// Demo
-Route::get('demo', function () {
-    return view('entrance::entrance.index');
-})->middleware(['web', 'auth']);
+Route::group(['middleware' => ['web'], 'namespace' => 'BrooksYang\Entrance\Controllers'], function () {
+
+    // Demo
+    Route::get('demo', function () {
+        return view('entrance::entrance.index');
+    })->middleware(['web', 'auth']);
+
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+});
--- packages/brooksyang/entrance/src/Admin/views/layouts/default.blade.php
+++ packages/brooksyang/entrance/src/Admin/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="" name="description">
     <meta content="" name="author">
-    <link href="ico/favicon.ico" rel="shortcut icon">
+    <link href="{{ url('ico/favicon.ico') }}" rel="shortcut icon">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
