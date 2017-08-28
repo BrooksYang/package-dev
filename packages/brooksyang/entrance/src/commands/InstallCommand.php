430e291e18435b51926aa22951d8f5412de6c3ed
BrooksYang <yangchangqing@easy2study.net>
2017-08-28 10:15:02 +0800

    install command

---
+++ packages/brooksyang/entrance/src/commands/InstallCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BrooksYang\Entrance;
+
+use BrooksYang\Entrance\Database\EntranceTablesSeeder;
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'entrance:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the entrance package';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $this->publishDatabase();
+    }
+
+    /**
+     * Create tables and seed it.
+     */
+    public function publishDatabase()
+    {
+        $this->call('migrate', ['--path' => __DIR__ . '/../database/migrations']);
+
+        $this->call('db:seed', ['--class' => EntranceTablesSeeder::class]);
+    }
+}
